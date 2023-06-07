46c7e15a0af5572b85413296c8b4cc7c62dfbddf
yzq-ai <127411394+yzq-ai@users.noreply.github.com>
2023-06-07 20:19:08 +0800

    测试类框架
    添加测试类框架

--- src/Application.cpp
+++ src/Application.cpp
@@ -17,6 +17,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"//测试
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -58,18 +60,7 @@ int main(void)
 
 
 	{ //限定作用域
-		// 顶点位置浮点型数组 
-		float positions[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f,  // 0 左下角顶点 (-50,-50)
-			 50.0f, -50.0f, 1.0f, 0.0f,  // 1 左上角顶点 (50,-50)
-			 50.0f,  50.0f, 1.0f, 1.0f,  // 2 右上角顶点 (50,50)
-			-50.0f,  50.0f, 0.0f, 1.0f   // 3 右下角顶点 (-50,50)
-		};
-		// 索引缓冲区所需索引数组 
-		unsigned int indices[] = {
-			0, 1, 2, // 取出positions数组的 0、1、2
-			2, 3, 0
-		};
+		
 
 		/*
 		混合:将输出颜色(判断着色器输出的颜色)和目标缓冲区已有的颜色结合
@@ -87,48 +78,6 @@ int main(void)
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));//使用混合
 		
 
-		VertexArray va;//使用顶点数组
-
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-
-		//设置索引缓冲区
-		IndexBuffer ib(indices, 6);
-
-		// 左:0 、 右:1280 、 下:0 、上:720
-		glm::mat4 proj = glm::ortho(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 1.0f);
-		// 相机位置          视图矩阵 
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));//向左移动100
-		// 模型矩阵 对象位置 
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));//向右移动200，向上移动200
-		// 模型视图投影矩阵
-		glm::mat4 mvp = proj * view * model;  
-
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		//shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP",mvp);
-
-
-
-		Texture texture("res/textures/avater.png");
-		texture.Bind();//绑定纹理，默认值是0
-		shader.SetUniform1i("u_Texture", 0);
-
-
-		//解绑了所有的东西
-		va.UnBind();
-		shader.UnBind();
-		vb.UnBind();
-		ib.UnBind();
-
-
 
 		Renderer renderer;
 
@@ -138,64 +87,27 @@ int main(void)
 
 
 		
-		const char* glsl_version = "#version 330";// 需要指定glsl版本, 也就是shader中的version
+		const char* glsl_version = "#version 330";// 需要指定GLSL版本, 也就是shader中的version
 		ImGui_ImplOpenGL3_Init(glsl_version);//将ImGui与OpenGL进行连接
 
+		test::TestClearColor test;
 
 		glm::vec3 tranlationA(200, 200, 0);
 		glm::vec3 tranlationB(400, 200, 0);
 
-		float r = 0.0f;
-		float increment = 0.005;
-
-
-		/* Loop until the user closes the window */
+		
 		while (!glfwWindowShouldClose(window)) {
-			/* Render here */
 			renderer.Clear();
 
+			test.OnUpdate(0.0f);
+            test.OnRender();
 
 			ImGui_ImplOpenGL3_NewFrame();//告诉ImGui开始一个新的渲染帧
 			ImGui_ImplGlfw_NewFrame();//告诉ImGui发生了新的GLFW窗口事件
 			ImGui::NewFrame();//ImGui准备开始一个新的界面元素布局帧
 			
 			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), tranlationA);
-				glm::mat4 mvp = proj * view * model;// 模型视图投影矩阵 
-
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
-			}
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), tranlationB);
-				glm::mat4 mvp = proj * view * model;
-
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
-			}
-
-		/*
-			if (r > 1.0f)
-				increment = -0.005;
-			if (r < 0.0f)
-				increment = 0.005f;
-
-			r += increment;
-		*/
-			{
-			
-
-				ImGui::Begin("ImGui");//开始一个名为"ImGui"的界面布局块
-				ImGui::SliderFloat3("TranlationA", &tranlationA.x, 0.0f, 960.0f);//添加了一个名为"TranslationA"的滑动条控件
-				ImGui::SliderFloat3("TranlationB", &tranlationB.x, 0.0f, 960.0f);//添加了一个名为"TranslationB"的滑动条控件
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-				//ImGui::Text(u8"世界");
-				ImGui::End();
+				test.OnImGuiRender();
 			}
 
 			ImGui::Render();//将当前的UI布局帧渲染为一个完整的UI图像
